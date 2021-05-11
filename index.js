a65a98041b7eae231a36523e7edd079661a58d2e
Apash <72939903+Apash00@users.noreply.github.com>
2021-05-11 11:08:47 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+console.log("Streamstatus by Apash")
+
+client.user.setActivity(`STATUE HERE`, {
+type: "STREAMING",
+url: "https://www.twitch.tv/twitch"})
+    .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'none'}`))
+    .catch(console.error);
+});
+
+client.login('TOKEN HERE');
